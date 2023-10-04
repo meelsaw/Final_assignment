03b099f1ce0b2776487ace3aff2c08783bda944e
meelsaw <mohamed.mamdouh.elsawy@gmail.com>
2023-10-04 01:06:13 +0100

    Fetching private key to decrypt

--- src/server/utils/decrept.py
+++ src/server/utils/decrept.py
@@ -1,8 +1,14 @@
 import rsa
 import os
+from dotenv import load_dotenv
 
 
 class Decryption:
     def decrypt_message(self, message):
-        decrypted_message = message + " decrypted"
-        return decrypted_message
+        load_dotenv()
+
+        private_key_location = os.getenv("PRIVATE_KEY")
+        private_key = rsa.PrivateKey.load_pkcs1(private_key_location)
+
+        decrypted_message = rsa.decrypt(message, private_key)
+        return decrypted_message.decode('utf-8')
