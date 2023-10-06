a394e491c2570dc758eec4c855f66568eae07c89
meelsaw <mohamed.mamdouh.elsawy@gmail.com>
2023-10-06 20:29:16 +0200

    creates helper methods to simplify convert file method and updated docstring

--- src/server/utils/file_handler.py
+++ src/server/utils/file_handler.py
@@ -4,8 +4,49 @@ import pickle
 
 
 class FileHandler:
+    def create_json(self, output_dict):
+        """
+        helper method creates a json file
+        :param output_dict: dict with the data to be written into json file
+        :return: json file
+        """
+        output = open("final_output.json", "w")
+        json.dump(output_dict, output, indent=4, sort_keys=False)
+        output.close()
+        return output
+
+    def create_xml(self, output_dict):
+        """
+        helper method creates a xml file
+        :param output_dict: dict with the data to be written into xml file
+        :return: xml file
+        """
+        xml_doc = et.Element("data")
+        for key, value in output_dict.items():
+            element = et.SubElement(xml_doc, key)
+            element.text = value
+        xml_file = 'final_output.xml'
+        tree = et.ElementTree(xml_doc)
+        tree.write(xml_file, encoding='utf-8', xml_declaration=True)
+        return xml_file
+
+    def create_binary(self, output_dict):
+        """
+        helper method creates a binary file
+        :param output_dict: dict with the data to be written into binary file
+        :return: binary file
+        """
+        bin_file = 'final_output.bin'
+        with open(bin_file, 'wb') as binary_file:
+            pickle.dump(output_dict, binary_file)
+        return bin_file
 
     def convert_file(self, **kwargs):
+        """
+        a method converts txt file to json/xml/binary
+        :param kwargs: file name and desired format
+        :return: json/xml/binary file
+        """
         file_obj = str()
         format_type = str()
         for key, value in kwargs.items():
@@ -18,30 +59,22 @@ class FileHandler:
             for line in f:
                 output_dict["content"] += line
         if format_type == "JSON":
-            final_output = open("final_output.json", "w")
-            json.dump(output_dict, final_output, indent=4, sort_keys=False)
-            final_output.close()
-            return final_output
+            return self.create_json(output_dict)
         elif format_type == "XML":
-            xml_doc = et.Element("data")
-            for key, value in output_dict.items():
-                element = et.SubElement(xml_doc, key)
-                element.text = value
-            xml_file_path = 'final_output.xml'
-            tree = et.ElementTree(xml_doc)
-            tree.write(xml_file_path, encoding='utf-8', xml_declaration=True)
-            return xml_file_path
+            return self.create_xml(output_dict)
         elif format_type == "BINARY":
-            bin_file_path = 'final_output.bin'
-            with open(bin_file_path, 'wb') as binary_file:
-                pickle.dump(output_dict, binary_file)
-            return bin_file_path
+            return self.create_binary(output_dict)
         else:
             message = "Invalid format request"
             print(message)
             return message
 
     def create_txt(self, input_str):
+        """
+        a method writes input str into txt
+        :param input_str: to be written into txt
+        :return: txt file
+        """
         try:
             file_name = "output_file.txt"
             with open(file_name, "w") as f:
